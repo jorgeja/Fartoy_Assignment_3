1853d98f81afbb07397e8fcdfc18d28d1519871e
jorgeja <mrjackwitz@gmail.com>
2017-10-27 14:46:26 +0200

    run_12 calculates timeconstants and k for nomoto modelsa at different dc values and simulation durations. nothing important in run_13

--- task_1_2/run_12.m
+++ task_1_2/run_12.m
@@ -24,11 +24,11 @@
 % WP.mat is a set of six waypoints that you need to use in the second part of
 % the assignment. The north position is given in the first row and the east
 % position in the second row. 
-
+clear all
+close all
 
 %%
 tstart=0;           % Sim start time
-tstop=10000;        % Sim stop time
 tsamp=10;           % Sampling time for how often states are stored. (NOT ODE solver time step)
                 
 p0=zeros(2,1);      % Initial position (NED)
@@ -37,26 +37,29 @@ psi0=0;             % Inital yaw angle
 r0=0;               % Inital yaw rate
 c=0;                % Current on (1)/off (0)
 
-K = zeros(10,1);
-Tc = zeros(10,1);
+K = zeros(3,1);
+T = zeros(3,1);
 ratio = zeros(10,1);
+timesteps = [1000,10000,100000]
 
-for delta_c = 1:1
+i = 1;
 
-    dc = 10*pi/180;
+for n = timesteps
+    tstop=n;        % Sim stop time
+    dc = 1*pi/180;
     
 sim MSFartoystyring % The measurements from the simulink model are automatically written to the workspace.
 
 %pathplotter(p(:,1),p(:,2),psi(:),tsamp,1,tstart,tstop,0,WP);
 
-r_deriv = r(2)/t(2);
-tNy = 0:1:200;
-T_vector = tNy*r_deriv;
-steadyState = r(length(r));
-
-Tc(delta_c) = steadyState/r_deriv;
-K(delta_c) = steadyState * Tc(delta_c) / dc;
-ratio(delta_c) = K(delta_c)/Tc(delta_c);
+% r_deriv = r(2)/t(2);
+% tNy = 0:1:200;
+% T_vector = tNy*r_deriv;
+% steadyState = r(length(r));
+% 
+% Tc(delta_c) = steadyState/r_deriv;
+% K(delta_c) = steadyState * Tc(delta_c) / dc;
+% ratio(delta_c) = K(delta_c)/Tc(delta_c);
 
 % Derive K and T for dc:
 tdata = t;
@@ -65,9 +68,14 @@ rdata = r*180/pi;
 % x(1) = 1/T; x(2) = K;
 x0 = [0.01, 0.1]';
 F = inline('exp(-tdata*x(1))*0 + x(2)*(1-exp(-tdata*x(1)))*5','x','tdata');
-x = lsqcurvefit(F,x0,tdata,rdata)
-disp('K:'); disp(K(1))
-disp('1/T:'); disp(1/Tc(1))
+x = lsqcurvefit(F,x0,tdata,rdata);
+
+T(i) = 1/x(1);
+K(i) = x(2);
+figure(i)
+plot(t,r,t,psi);
+legend('r','psi','dc');
+
 % figure(delta_c)
 % plot(t,r);
 % hold on
@@ -75,5 +83,10 @@ disp('1/T:'); disp(1/Tc(1))
 % hold on
 % plot(ones(1,200)*steadyState);
 % legend('yaw rate','timeconstantvector','steadystate');
-
+ i = i + 1;
 end
+disp('25 grader');
+disp('K:');
+disp(K);
+disp('T:')
+disp(T);
