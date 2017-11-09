d88e29c7430bf593ceba7bea12e1934e8a33d903
jorgeja <mrjackwitz@gmail.com>
2017-11-09 22:17:32 +0100

    1,8 zip is ready for handin

--- task_1_8/run_task_18.m
+++ task_1_8/run_task_18.m
@@ -1,30 +1,3 @@
-%% Information 
-% This file is only an example of how you can start the simulation. The
-% sampling time decides how often you store states. The execution  time
-% will increase if you reduce the sampling time.
-
-% Please note that the file "pathplotter.m" (only used in the second part
-% of the assignment) shows the ship path during the path following and
-% target tracking part of the assignment. It can be clever to adjust the sampling
-% time when you use that file because it draws a sketch of the ship in the
-% North-East plane at each time instant. Having a small sampling time will
-% lead to a lot over overlap in the ship drawing. 
-
-% You should base all of your simulink models on the MSFartoystyring model
-% and extend that as you solve the assignment. For your own sake, it is
-% wise to create a new model and run file for each task. That is
-% especially important in the problems you need to hand in since the files
-% you deliver only should create the desired result in that task.
-
-% The msfartoystyring.m file includes the ship model. You are not allowed
-% to change anything within that file. You need to include that file in
-% every folder where you have a simulink model based on
-% "MSFartoystyring.slx". 
-
-% WP.mat is a set of six waypoints that you need to use in the second part of
-% the assignment. The north position is given in the first row and the east
-% position in the second row. 
-
 close all;
 clear all;
 
