d68dcb7c0ab9e4cb68e8b93004f79d614e3b58c6
Sveinung Haugane <sveiaug@gmail.com>
2017-11-09 13:28:14 +0100

    Added integration to course control

--- task_2_7/normalize.m
+++ task_2_7/normalize.m
@@ -1,11 +1,13 @@
 function angle_out = normalize(angle_in)
-angle_in = mod(angle_in,2*pi);
-if angle_in > pi
-    angle_out = angle_in - 2*pi;
-elseif angle_in < - pi 
-        angle_out = angle_in + 2*pi;
-else
-    angle_out = angle_in;
+for i = 1:length(angle_in)
+    angle_in(i) = mod(angle_in(i),2*pi);
+    if angle_in(i) > pi
+        angle_out(i) = angle_in(i) - 2*pi;
+    elseif angle_in(i) < - pi 
+        angle_out(i) = angle_in(i) + 2*pi;
+    else
+        angle_out(i) = angle_in(i);
+    end
 end
 end
 
